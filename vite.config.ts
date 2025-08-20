72c8875c2e10cd32514b27d12a8a5de827e0a3b1
Ibrahim <ibusalat@gmail.com>
2025-08-21 00:12:35 +0100

    Vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  base: '/',
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  // base: '/',
   plugins: [
     react(),
     mode === 'development' &&
